e95f1d72da91933dcc16e415f5521fd5e7e34366
abhishekbhave26 <abhishekbhave26@gmail.com>
2024-01-07 21:19:58 -0800

    Reindent all files

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ const dotenv = require('dotenv');
 require('custom-env').env();
 
 if (process.env.NODE_ENV === 'dev') {
-  dotenv.config({ path: '.env.dev' });
+	dotenv.config({ path: '.env.dev' });
 } else if (process.env.NODE_ENV === 'prod') {
-  dotenv.config({ path: '.env.prod' });
+	dotenv.config({ path: '.env.prod' });
 } else if (process.env.NODE_ENV === 'test') {
-  dotenv.config({ path: '.env.test' });
+	dotenv.config({ path: '.env.test' });
 }
 
 const app = express();
@@ -20,22 +20,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 if (process.env.NODE_ENV === 'dev') {
-  app.use(morgan('dev'));
+	app.use(morgan('dev'));
 }
 
 const uriConnStr = process.env.ATLAS_URI;
 
 console.log(uriConnStr);
 
-mongoose.connect(uriConnStr, { useNewUrlParser: true, useCreateIndex: true }
-);
+mongoose.connect(uriConnStr, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 
 //use gmail email id for mongo db
 connection.once('open', () => {
-  if (process.env.NODE_ENV === 'dev') {
-    console.log("MongoDB database connection established successfully");
-  }
+	if (process.env.NODE_ENV === 'dev') {
+		console.log("MongoDB database connection established successfully");
+	}
 })
 
 const optionRouter = require('./routes/option');
@@ -43,9 +42,9 @@ const optionRouter = require('./routes/option');
 app.use('/', optionRouter);
 
 const server = app.listen(port, () => {
-  if (process.env.NODE_ENV === 'dev') {
-    console.log(`Server is running on port: ${port}`);
-  }
+	if (process.env.NODE_ENV === 'dev') {
+		console.log(`Server is running on port: ${port}`);
+	}
 });
 
 module.exports = server;
